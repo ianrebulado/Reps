1f2bd9c9fbfb556154fc798e5602252c045c4dd1
ianrebulado <eilrebulado@gmail.com>
2024-05-29 20:44:23 +0800

    fix: change table column age to dob

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_021816) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_080903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,12 +42,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_021816) do
     t.datetime "remember_created_at"
     t.string "first_name"
     t.string "last_name"
-    t.integer "age"
     t.decimal "weight"
     t.decimal "height"
     t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
