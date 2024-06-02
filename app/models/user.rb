779872fd415ad4689f08874f6a8ff7f446b22476
ianrebulado <eilrebulado@gmail.com>
2024-06-02 20:04:50 +0800

    feat: create helper methods for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :workouts, dependent: :destroy
-  has_many :planned_exercises, through: :workouts
-  has_many :actual_exercises, through: :planned_exercises
+
+  def age
+    now = Time.now.localtime.to_date
+    dob = date_of_birth
+    age = now.year - dob.year
+    age -= 1 if now.month < dob.month || (now.month == dob.month && now.day < dob.day)
+    age
+  end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
   
 end
