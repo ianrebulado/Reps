662193378275d61fb3d7442e6f8e152e58803b30
ianrebulado <eilrebulado@gmail.com>
2024-06-03 13:33:56 +0800

    feat: add exercises table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_03_052114) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_03_053058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.string "body_part"
+    t.string "url"
+    t.text "instructions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
