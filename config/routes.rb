592bba56b796d80514d36fd550137fad47c739da
ianrebulado <eilrebulado@gmail.com>
2024-06-08 11:23:09 +0800

    chore: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { 
-    sessions: "users/sessions",
+  # Devise routes for user authentication
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Health check route
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  root "home#index"
+  # Root route
+  root 'home#home'
 
-  get "home", to: "home#home"
-  get "profile", to: "home#show"
-  get "programs", to: "home#programs"
+  # Custom home routes
+  get 'profile', to: 'home#show'
+  get 'programs', to: 'home#programs'
+  get 'home', to: 'home#home'
+  patch 'profile/update', to: 'home#update', as: :update_profile
+  get 'edit', to: 'home#edit'
+  get 'create_default_workout_and_redirect', to: 'home#create_default_workout_and_redirect'
+
+  # Resourceful routes for workouts and nested resources for exercises
+  resources :workouts do
+    resources :exercises
+  end
 end
