0b1158e604508264b80f610135c4a241f31e43f0
ianrebulado <eilrebulado@gmail.com>
2024-06-08 11:22:01 +0800

    chore: table associations

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,10 @@
 class Workout < ApplicationRecord
   belongs_to :user
-  has_many :exercises, dependent: :destroy
+  has_many :exercises
+  accepts_nested_attributes_for :exercises
+  
+  def self.for_today(user)
+    where(user_id: user.id)
+      .where("EXTRACT(DOW FROM created_at) = ?", Time.current.strftime("%w"))
+  end
 end
